6519509735fc62f3396bb79a50ce9c560f5f3207
Nitheesh <nitheeshmashraf@gmail.com>
2017-12-26 12:19:54 +0400

    till routes in components

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';  
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';      
 
 @Component({
   selector: 'app-employee',
@@ -9,11 +10,15 @@ import { ActivatedRoute } from '@angular/router';
 export class EmployeeComponent implements OnInit {
 
 	empName:string;
-  constructor(private route: ActivatedRoute) { 
+  constructor(private route: ActivatedRoute, private router: Router) { 
     this.route.params.subscribe(res => this.empName=res.name);
   }
 
   ngOnInit() {
+  }
+
+  sendMeHome() {
+    this.router.navigate(['']);
   }
 
 }
